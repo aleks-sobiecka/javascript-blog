a179b3c4071a69cf78069f93e57af96a4842ed30
Aleksandra Sobiecka <aleks.sobiecka@gmail.com>
2023-05-18 18:21:32 +0200

    Get 'href' attribute from the clicked link

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
   const titleClickHandler = function(event){
+    event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
 
@@ -11,7 +12,7 @@
     activeLink.classList.remove('active');
   }
 
-  /* [IN PROGRESS] add class 'active' to the clicked link */
+  /* [DONE] add class 'active' to the clicked link */
   console.log('clickedElement:', clickedElement);
 
   clickedElement.classList.add('active');
@@ -23,7 +24,9 @@
     activeArticle.classList.remove('active');
   }
 
-  /* get 'href' attribute from the clicked link */
+  /* [IN PROGRESS] get 'href' attribute from the clicked link */
+  const articleSelector = clickedElement.getAttribute("href");
+  console.log(articleSelector);
 
   /* find the correct article using the selector (value of 'href' attribute) */
 
