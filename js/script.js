f34f20d2aab79f1ca2582b393b0dff5a499593e1
Aleksandra Sobiecka <aleks.sobiecka@gmail.com>
2023-05-21 13:20:10 +0200

    Add algorithm to functions authorClickHandler and addClickListenersToAuthors

--- js/script.js
+++ js/script.js
@@ -241,6 +241,31 @@
 
   generateAuthors();
 
+  const authorClickHandler = function(event){
+    /* prevent default action for this event */
+    /* make new constant named "clickedElement" and give it the value of "this" */
+    /* make a new constant "href" and read the attribute "href" of the clicked element */
+    /* make a new constant "author" and extract author from the "author" constant */
+    /* find all author links with class active */
+    /* START LOOP: for each active author link */
+      /* remove class active */
+    /* END LOOP: for each active author link */
+    /* find all author links with "href" attribute equal to the "href" constant */
+    /* START LOOP: for each found author link */
+      /* add class active */
+    /* END LOOP: for each found author link */
+    /* execute function "generateTitleLinks" with article selector as argument */
+  };
+
+  const addClickListenersToAuthors = function(){
+    /* find all links to authors */
+    /* START LOOP: for each link */
+      /* add authorClickHandler as event listener for that link */
+    /* END LOOP: for each link */
+  };
+
+  addClickListenersToAuthors();
+
 
 
 }
